e8ab0264d4235b16842537dd14857750617eb3af
faizan ul haq <mfaizanulhaq1@gmail.com>
2020-06-25 02:45:25 +0500

    Updates

---
+++ precache-manifest.d8daa55abcf0f0c218d77512fa98d08e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "521acb5027c00315ee78e2c134ee29cb",
+    "url": "/pwa-react-excercise/index.html"
+  },
+  {
+    "revision": "885cbe5ab336a6b5685f",
+    "url": "/pwa-react-excercise/static/css/2.7a3f122f.chunk.css"
+  },
+  {
+    "revision": "af80e003a68733da28ba",
+    "url": "/pwa-react-excercise/static/css/main.a906b4e6.chunk.css"
+  },
+  {
+    "revision": "885cbe5ab336a6b5685f",
+    "url": "/pwa-react-excercise/static/js/2.8e6ea57e.chunk.js"
+  },
+  {
+    "revision": "476c15f2ad62ef14880973b98d41458b",
+    "url": "/pwa-react-excercise/static/js/2.8e6ea57e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af80e003a68733da28ba",
+    "url": "/pwa-react-excercise/static/js/main.0f106b9d.chunk.js"
+  },
+  {
+    "revision": "552216392e6371b66435",
+    "url": "/pwa-react-excercise/static/js/runtime-main.e1d6ebda.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/pwa-react-excercise/static/media/SEGA.12a3db8b.woff"
+  }
+]);
