d92302886ae90d805ad280553e8bcebe614233b6
faizan ul haq <mfaizanulhaq1@gmail.com>
2020-06-25 19:16:22 +0500

    Updates

---
+++ precache-manifest.351795fd9dbde65761d5e85afecc4aa7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af0a7feea2a1000ec47c593514f3dd56",
+    "url": "/pwa-react-excercise/index.html"
+  },
+  {
+    "revision": "ca5411f3cb874653a63d",
+    "url": "/pwa-react-excercise/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ea77df437ff9853465cd",
+    "url": "/pwa-react-excercise/static/css/main.a906b4e6.chunk.css"
+  },
+  {
+    "revision": "ca5411f3cb874653a63d",
+    "url": "/pwa-react-excercise/static/js/2.8bf57d0e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pwa-react-excercise/static/js/2.8bf57d0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea77df437ff9853465cd",
+    "url": "/pwa-react-excercise/static/js/main.0ff987bb.chunk.js"
+  },
+  {
+    "revision": "552216392e6371b66435",
+    "url": "/pwa-react-excercise/static/js/runtime-main.e1d6ebda.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/pwa-react-excercise/static/media/SEGA.12a3db8b.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pwa-react-excercise/precache-manifest.efd78aba6a0f904a71b75dd523a1f3b8.js"
+  "/pwa-react-excercise/precache-manifest.351795fd9dbde65761d5e85afecc4aa7.js"
 );
 
 self.addEventListener('message', (event) => {
